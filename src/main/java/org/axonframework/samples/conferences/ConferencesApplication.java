688e12ac308721d412b7b2ca1d8afaa69958c6fe
smcvb <smcvbeelen@gmail.com>
2017-10-23 16:07:06 +0200

    Initial commit

---
+++ src/main/java/org/axonframework/samples/conferences/ConferencesApplication.java
@@ -0,0 +1,12 @@
+package org.axonframework.samples.conferences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferencesApplication.class, args);
+	}
+}
---
+++ src/test/java/org/axonframework/samples/conferences/ConferencesApplicationTests.java
@@ -0,0 +1,16 @@
+package org.axonframework.samples.conferences;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConferencesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
